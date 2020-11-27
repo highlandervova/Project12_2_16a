b3af46285fcccf00732ac7999bf250caf12b7670
pani-marinka <pani_marinka@ukr.net>
2020-11-27 03:07:56 +0200

    panarina

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,15 @@
 package main;
 
+import java.util.Arrays;
+
 public class Main {
-
     public static void main(String[] args) {
+        int[] arr = {2,-50,15,68,0,15,51};
 
+        System.out.println(Arrays.toString(arr));
+        Sorter sorter = new MinMaxSorter();
+        arr = sorter.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/main/MinMaxSorter.java
+++ src/main/MinMaxSorter.java
@@ -1,7 +1,29 @@
 package main;
 
-public class MinMaxSorter {
-    public static void main(String[] args) {
+public class MinMaxSorter implements Sorter{
+    @Override
+    public int[] sort(int[] arr) {
 
+        int k,j;
+
+        int[] tmp = new int[arr.length];
+
+        for (int i = 0; i <  arr.length; i++) {
+            k=arr[0];
+            j=0;
+            for (int n = 1; n < arr.length; n++) {
+                if (k > arr[n]) {
+                    k = arr[n];
+                    j = n;
+                }
+            }
+
+//comment
+            tmp[i]=k;
+            arr[j]=Integer.MAX_VALUE;
+        }
+
+        arr=tmp;
+        return arr;
     }
 }
